78db5ecc007354dd3cc04b52cfc6efbddeb76c62
Constantine <aspid812@gmail.com>
2021-05-17 13:01:28 +0300

    Add application skeleton

---
+++ src/main/kotlin/dev/aspid812/softmachdemo/SoftMachinaDemoApplication.kt
@@ -0,0 +1,11 @@
+package dev.aspid812.softmachdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SoftMachinaDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SoftMachinaDemoApplication>(*args)
+}
---
+++ src/test/kotlin/dev/aspid812/softmachdemo/SoftMachinaDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.aspid812.softmachdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SoftMachinaDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
